c0d5ac37f182242c29a12831a49a4e4724cff142
Bebe Jane <bjornthief@gmail.com>
2022-10-22 17:07:16 +0200

    updatez

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+import * as utils from './src/utils';
+import { apiQuery, SEOQuery } from './src/api';
+export { utils, apiQuery, SEOQuery };
+export { default as usePreviousRoute } from './src/hooks/usePreviousRoute';
+export { default as useScrollInfo } from './src/hooks/useScrollInfo';
+export { default as useTransitionFix } from './src/hooks/useTransitionFix';
+export { default as withRevalidate } from './src/dato/hoc/withRevalidate';
+export { default as withGlobalProps } from './src/hoc/withGlobalProps';
+export { default as withPreview } from './src/hoc/withPreview';
+export { default as Markdown } from './src/dato/components/Markdown';
+export { default as DatoSEO } from './src/dato/components/DatoSEO';
