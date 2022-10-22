a52b487f905b7499146b557f9394495eccf78574
Bebe Jane <bjornthief@gmail.com>
2022-10-22 15:59:39 +0200

    update

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
 export { default as usePreviousRoute } from "./src/hooks/usePreviousRoute";
 export { default as useScrollInfo } from "./src/hooks/useScrollInfo";
 export { default as useTransitionFix } from "./src/hooks/useTransitionFix";
-export { default as withGlobalProps } from "./src/hoc/withGlobalProps";
 export { default as withRevalidate } from "./src/dato/hoc/withRevalidate";
+export { default as withGlobalProps } from "./src/hoc/withGlobalProps";
+export { default as withPreview } from "./src/hoc/withPreview";
 export { default as Markdown } from "./src/dato/components/Markdown";
 export { default as DatoSEO } from "./src/dato/components/DatoSEO";
 import * as utils from "./src/utils";
