1d976611e2fca68daeee5ddb6c330e7e14cbf13e
Bebe Jane <bjornthief@gmail.com>
2022-10-25 17:41:05 +0200

    updatez

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ export { default as useApiQuery} from './hooks/useApiQuery.js'
 export { default as usePreviousRoute } from './hooks/usePreviousRoute.js'
 export { default as useScrollInfo } from './hooks/useScrollInfo.js'
 export { default as useTransitionFix } from './hooks/useTransitionFix.js'
+export * from './components/index.js'
 export { apiQuery, SEOQuery }
