8ec913135cf27b66ada9ab9dd65010bd37a590b4
Bebe Jane <bjornthief@gmail.com>
2022-10-25 19:18:23 +0200

    updatez

--- src/components/DatoSEO.tsx
+++ src/components/DatoSEO.tsx
@@ -1,4 +1,4 @@
-import { NextSeo, DefaultSeo } from 'next-seo';
+import { NextSeo, DefaultSeo } from 'next-seo/lib/index.js';
 
 const DatoSEO = ({seo = {}, site = {}, pathname, title, subtitle, description, noindex = false} : any) => {
   
