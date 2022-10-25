5f62ecbcff75a9382642e0942c81e5820e0ce3da
Bebe Jane <bjornthief@gmail.com>
2022-10-25 17:04:51 +0200

    updatez

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { apiQuery, SEOQuery } from './api.js'
 export { default as withGlobalProps } from './hoc/withGlobalProps.js'
 export { default as withPreview } from './hoc/withPreview.js'
 export { default as withRevalidate } from './hoc/withRevalidate.js'
+export { default as useApiQuery} from './hooks/useApiQuery.js'
+export { default as usePreviousRoute } from './hooks/usePreviousRoute.js'
+export { default as useScrollInfo } from './hooks/useScrollInfo.js'
+export { default as useTransitionFix } from './hooks/useTransitionFix.js'
 export { apiQuery, SEOQuery }
