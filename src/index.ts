12a388f55bfa7269fda1cec884a90a73cbbd4118
Bebe Jane <bjornthief@gmail.com>
2022-10-25 16:56:53 +0200

    updatez

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { apiQuery, SEOQuery } from './api.js'
 export { default as withGlobalProps } from './hoc/withGlobalProps.js'
 export { default as withPreview } from './hoc/withPreview.js'
+export { default as withRevalidate } from './hoc/withRevalidate.js'
 export { apiQuery, SEOQuery }
