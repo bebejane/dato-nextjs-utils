e6b90338ea6d6ccf46b47a05bc8825611a46eca1
Bebe Jane <bjornthief@gmail.com>
2022-10-25 17:27:33 +0200

    updatez

---
+++ dist/components/index.d.ts
@@ -0,0 +1,3 @@
+import DatoMarkdown from "./DatoMarkdown.jsx";
+import DatoSEO from "./DatoSEO.jsx";
+export { DatoMarkdown, DatoSEO };
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import DatoMarkdown from "./DatoMarkdown.jsx";
+import DatoSEO from "./DatoSEO.jsx";
+
+export {
+  DatoMarkdown,
+  DatoSEO
+}
