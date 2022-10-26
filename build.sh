7ad9afa5350628cdd7f23eabcb6d1302f45bbe45
Bebe Jane <bjornthief@gmail.com>
2022-10-26 14:07:51 +0200

    updatez

--- build.sh
+++ build.sh
@@ -3,16 +3,18 @@ packages=(
   hooks
   components
   api
-  index
 )
 
 rm -rf ${packages[@]}
+rm -rf index.*
 
 echo Target cleared...
 
 for package in "${packages[@]}"; do
-  pnpm microbundle -i src/$package.ts -o $package/index.js --tsconfig 'tsconfig.json' --name ${package} &
+  pnpm microbundle -i src/$package.ts -o $package/index.js --tsconfig 'tsconfig.json' --name ${package} --jsx React.createElement &
 done
+
+pnpm microbundle -i src/index.ts -o ./index.js --tsconfig 'tsconfig.json' --name index --jsx React.createElement
 
 wait
 
