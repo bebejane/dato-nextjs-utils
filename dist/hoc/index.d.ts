84d2f6e5a58b58d133156830b212e8a3ac554bb3
Bebe Jane <bjornthief@gmail.com>
2022-10-26 13:41:52 +0200

    lkj

--- dist/hoc/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as withGlobalProps } from './withGlobalProps.js';
-export { default as withPreview } from './withPreview.js';
-export { default as withRevalidate } from './withRevalidate.js';
