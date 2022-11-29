901f40d0d3b9ba5c52b38f72712fac49acbd5c17
Bebe Jane <bjornthief@gmail.com>
2022-11-29 13:40:51 +0100

    updatez

--- src/hoc/withRevalidate.ts
+++ src/hoc/withRevalidate.ts
@@ -2,30 +2,30 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { buildClient } from '@datocms/cma-client';
 
 export const basicAuth = (req: NextApiRequest) => {
-  
+
   const basicAuth = req.headers.authorization
-  if (!basicAuth) 
+  if (!basicAuth)
     return true;
-    
+
   const auth = basicAuth.split(' ')[1]
   const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
   return user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD
 }
 
-const recordFromPayload = async (payload: any) : Promise<any> => {
+const recordFromPayload = async (payload: any): Promise<any> => {
 
   const modelId = payload?.relationships?.item_type?.data?.id
 
-  if (!modelId) 
+  if (!modelId)
     throw 'Model id not found in payload!'
-  
+
   console.log('resolve modelId', modelId)
-  const client = buildClient({ apiToken: process.env.NEXT_PUBLIC_GRAPHQL_API_TOKEN, requestTimeout:3000 })
+  const client = buildClient({ apiToken: process.env.GRAPHQL_API_TOKEN || process.env.NEXT_PUBLIC_GRAPHQL_API_TOKEN, requestTimeout: 3000 })
   const models = await client.itemTypes.list()
   const model = models.find(m => m.id === modelId)
   const records = await client.items.list({ filter: { type: model.api_key, fields: { id: { eq: payload.id } } } })
   const record = records[0]
-  
+
   if (!record)
     throw `No record found with modelId: ${modelId}`
 
@@ -34,7 +34,7 @@ const recordFromPayload = async (payload: any) : Promise<any> => {
 
 }
 
-export default function withRevalidate(callback:(record:any, revalidate : (paths:string[]) => Promise<void> ) => Promise<void>) : (req: NextApiRequest, res: NextApiResponse) => void {
+export default function withRevalidate(callback: (record: any, revalidate: (paths: string[]) => Promise<void>) => Promise<void>): (req: NextApiRequest, res: NextApiResponse) => void {
 
   return async (req: NextApiRequest, res: NextApiResponse) => {
 
@@ -49,7 +49,7 @@ export default function withRevalidate(callback:(record:any, revalidate : (paths
     const record = await recordFromPayload(payload)
 
     callback(record, async (paths) => {
-      try{
+      try {
         if (!paths.length)
           throw 'Nothing to revalidate';
 
@@ -57,11 +57,11 @@ export default function withRevalidate(callback:(record:any, revalidate : (paths
         await Promise.all(paths.map(p => res.revalidate(p)))
         console.log('revalidating done!')
         return res.json({ revalidated: true, paths })
-      }catch(err){
+      } catch (err) {
         console.error(err)
         return res.json({ revalidated: false, err })
       }
-      
+
     })
   }
 }
