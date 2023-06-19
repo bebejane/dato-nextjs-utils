3c0a8b1945efcea258f98fbec8192d9ec28c6b8d
Bebe Jane <bjornthief@gmail.com>
2023-06-19 13:25:58 +0200

    updatez

--- hoc/withBackup.d.ts
+++ hoc/withBackup.d.ts
@@ -1,3 +1,3 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-export declare const basicAuth: (req: NextApiRequest) => boolean;
-export default function withBackup(req: NextApiRequest, res: NextApiResponse): Promise<void>;
+declare const withBackup: (req: NextApiRequest, res: NextApiResponse<any>) => void;
+export default withBackup;
