6a0ba730faa9b40f35680eec1c80cad56e86429a
Bebe Jane <bjornthief@gmail.com>
2023-06-19 12:17:18 +0200

    updatez

--- src/hoc/withBasicAuth.ts
+++ src/hoc/withBasicAuth.ts
@@ -5,10 +5,10 @@ export default function withBasicAuth(callback: (req: NextApiRequest, res: NextA
   return async (req: NextApiRequest, res: NextApiResponse) => {
 
     const basicAuth = req.headers.authorization
-
+    console.log(req.headers)
     if (!basicAuth)
       return res.status(401).send('Access denied')
-    console.log(process.env.BASIC_AUTH_USER, process.env.BASIC_PASSWORD)
+    console.log(process.env.BASIC_AUTH_USER, process.env.BASIC_AUTH_PASSWORD)
     const auth = basicAuth.split(' ')[1]
     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
     const isAuthorized = user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD
