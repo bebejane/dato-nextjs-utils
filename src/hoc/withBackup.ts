e8424c9f7cfb383266b36798c4078d355a65d49f
Bebe Jane <bjornthief@gmail.com>
2023-06-19 11:22:15 +0200

    updatez

--- src/hoc/withBackup.ts
+++ src/hoc/withBackup.ts
@@ -28,20 +28,32 @@ export default async function withBackup(req: NextApiRequest, res: NextApiRespon
   if (!process.env.DATOCMS_API_TOKEN)
     return res.status(401).send('DATOCMS_API_TOKEN not set in .env')
 
+  const maxBackups = req.query.max ? parseInt(req.query.max as string) : 1
+  const backupPrefix = 'auto-backup-'
   const client = buildClient({ environment: process.env.DATOCMS_ENVIRONMENT, apiToken: process.env.DATOCMS_API_TOKEN, requestTimeout: 3000 })
-  const backups = (await client.environments.list()).filter(e => e.id.startsWith('auto-backup'))
-  const today = new Date().toISOString().split('T')[0]
-  const name = `auto-backup ${today}`
-  console.log('Last backup was: ', backups.sort((a, b) => a.id.replace('auto-backup ', '') > b.id.replace('auto-backup ', '') ? -1 : 1)[0]?.id)
+  const backups = (await client.environments.list()).filter(e => e.id.startsWith('auto-backup') && e.meta.primary === false).sort((a, b) => a.id.replace(backupPrefix, '') > b.id.replace(backupPrefix, '') ? -1 : 1)
+  const today = new Date().toISOString().replace('T', '-').replaceAll(':', '-').replace('Z', '').split('.')[0]
+  const name = `auto-backup-${today}`
 
+  console.log('Last backup was: ', backups[0]?.id)
   console.log('Creating backup...', name)
-  const backup = await client.environments.fork(process.env.DATOCMS_ENVIRONMENT, { name })
 
-  for (let i = 0; i < backups.length; i++) {
-    console.log('Deleting old backup...', backups[i].id)
-    await client.environments.destroy(backups[i].id)
+  try {
+    const backup = await client.environments.fork(process.env.DATOCMS_ENVIRONMENT, { id: name }, {
+      immediate_return: false,
+      fast: false,
+      force: true
+    })
+
+    for (let i = 0; i < backups.reverse().slice(maxBackups - 1).length; i++) {
+      console.log('Deleting old backup...', backups[i].id)
+      await client.environments.destroy(backups[i].id)
+    }
+    console.log('Backup done!', backup.id)
+  } catch (e) {
+    console.error(e)
+    return res.status(401).send(`Backup failed: ${e.message}`)
   }
-  console.log('Backup done!', backup.id)
 
   return res.status(200).send('OK')
 
