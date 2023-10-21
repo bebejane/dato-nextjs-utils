3384db96fdcfd5df986dda3db28045775f55773c
Bebe Jane <bjornthief@gmail.com>
2023-10-21 10:49:24 +0200

    updatez

--- src/utils/tests.ts
+++ src/utils/tests.ts
@@ -100,13 +100,7 @@ export const testResultsToHtml = (results: TestResult[]) => {
           </thead>
           <tbody>
             ${results.map(r => `
-              <tr title="
-                Web Previews\n
-                ${r.previews?.map(p => `${p.label}: ${p.url}`).join('\n')}
-                \n\n
-                Revalidate\n
-                ${r.revalidate?.paths.join('\n')}
-              ">
+              <tr title="Web Previews\n${r.previews?.map(p => `${p.label}: ${p.url}`).join('\n')}\n\nRevalidate\n${r.revalidate?.paths.join('\n')}">
                 <td>${r.model}</td>
                 <td class="center">${r.previews ? 'x' : '-'}</td>
                 <td class="center">${r.revalidate ? 'x' : '-'}</td>
