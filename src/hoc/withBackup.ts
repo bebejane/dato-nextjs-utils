1ca039458259d5cf5b463f9ef242c011e4e6d3e6
Bebe Jane <bjornthief@gmail.com>
2023-10-25 23:25:39 +0200

    updatez

--- src/hoc/withBackup.ts
+++ src/hoc/withBackup.ts
@@ -9,18 +9,18 @@ const withBackup = withBasicAuth(async (req: NextApiRequest, res: NextApiRespons
   if (!process.env.DATOCMS_API_TOKEN)
     return res.status(500).send('DATOCMS_API_TOKEN not set in .env')
 
-  const maxBackups = req.query.max ? parseInt(req.query.max as string) : 1
-  const backupPrefix = 'auto-backup-'
-  const client = buildClient({ environment: process.env.DATOCMS_ENVIRONMENT, apiToken: process.env.DATOCMS_API_TOKEN, requestTimeout: 3000 })
-  const backups = (await client.environments.list()).filter(e => e.id.startsWith('auto-backup') && e.meta.primary === false).sort((a, b) => a.id.replace(backupPrefix, '') > b.id.replace(backupPrefix, '') ? -1 : 1)
-  const today = new Date().toISOString().replace('T', '-').replaceAll(':', '-').replace('Z', '').split('.')[0]
-  const name = `auto-backup-${today}`
-
-  console.log('Last backup was: ', backups[0]?.id ?? 'none')
-  console.log('Max backups: ', maxBackups)
-  console.log('Creating backup...', name)
-
   try {
+
+    const maxBackups = req.query.max ? parseInt(req.query.max as string) : 1
+    const backupPrefix = 'auto-backup-'
+    const client = buildClient({ environment: process.env.DATOCMS_ENVIRONMENT, apiToken: process.env.DATOCMS_API_TOKEN, requestTimeout: 3000 })
+    const backups = (await client.environments.list()).filter(e => e.id.startsWith('auto-backup') && e.meta.primary === false).sort((a, b) => a.id.replace(backupPrefix, '') > b.id.replace(backupPrefix, '') ? -1 : 1)
+    const today = new Date().toISOString().replace('T', '-').replaceAll(':', '-').replace('Z', '').split('.')[0]
+    const name = `auto-backup-${today}`
+
+    console.log('Last backup was: ', backups[0]?.id ?? 'none')
+    console.log('Max backups: ', maxBackups)
+    console.log('Creating backup...', name)
 
     await client.environments.fork(process.env.DATOCMS_ENVIRONMENT, { id: name }, {
       immediate_return: false,
@@ -39,7 +39,7 @@ const withBackup = withBasicAuth(async (req: NextApiRequest, res: NextApiRespons
     console.log('Backup done!')
     return res.status(200).send(`Backup done ${process.env.DATOCMS_ENVIRONMENT} > ${name}`)
   } catch (e) {
-    console.error(e)
+    console.log(e)
     return res.status(500).send(`Backup failed: ${e.message}`)
   }
 
