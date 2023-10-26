bc1d15acfa03e8035a50cf3a92c957f12c019ccb
Bebe Jane <bjornthief@gmail.com>
2023-10-26 16:23:10 +0200

    updatez

--- hoc/withTests.d.ts
+++ hoc/withTests.d.ts
@@ -1,2 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 export default function withTests(req: NextApiRequest, res: NextApiResponse): Promise<void>;
+declare type PreviewLink = {
+    label: string;
+    url: string;
+};
+declare type RevalidateResponse = {
+    revalidated: boolean;
+    paths: string[];
+    delays: number;
+    event_type: string;
+};
+declare type TestResult = {
+    model: string;
+    previews?: PreviewLink[];
+    revalidate?: RevalidateResponse;
+};
+export declare function testApiEndpoints(): Promise<TestResult[]>;
+export declare const testResultsToString: (results: TestResult[]) => string;
+export declare const testResultsToHtml: (results: TestResult[]) => string;
+export {};
