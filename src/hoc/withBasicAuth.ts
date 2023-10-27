11290f6e141ad15a4f0e25f0365c6b8ff2be56f1
Bebe Jane <bjornthief@gmail.com>
2023-10-27 20:10:15 +0200

    updatez

--- src/hoc/withBasicAuth.ts
+++ src/hoc/withBasicAuth.ts
@@ -4,10 +4,10 @@ export default function withBasicAuth(callback: (req: NextApiRequest, res: NextA
 
   return async (req: NextApiRequest, res: NextApiResponse) => {
 
-    if (process.env.NODE_ENV === 'development')
+    if (req.method === 'OPTIONS')
       return callback(req, res)
 
-    if (req.method === 'OPTIONS')
+    if (process.env.NODE_ENV === 'development')
       return callback(req, res)
 
     const basicAuth = req.headers.authorization
