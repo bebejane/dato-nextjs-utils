3e7b70db5388a80da8d353a91e271628b89c22e4
Bebe Jane <bjornthief@gmail.com>
2023-10-27 16:05:34 +0200

    updatez

--- src/hoc/withBasicAuth.ts
+++ src/hoc/withBasicAuth.ts
@@ -12,8 +12,10 @@ export default function withBasicAuth(callback: (req: NextApiRequest, res: NextA
     if (!process.env.BASIC_AUTH_USER || !process.env.BASIC_AUTH_PASSWORD)
       throw new Error('Basic auth not set up in .env')
 
-    if (!basicAuth || !process.env.BASIC_AUTH_USER || !process.env.BASIC_AUTH_PASSWORD)
+    if (!basicAuth || !process.env.BASIC_AUTH_USER || !process.env.BASIC_AUTH_PASSWORD) {
+      res.setHeader('WWW-Authenticate', 'Basic realm="NodeJs", charset="UTF-8"')
       return res.status(401).send('Access denied')
+    }
 
     const auth = basicAuth.split(' ')[1]
     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
@@ -21,10 +23,8 @@ export default function withBasicAuth(callback: (req: NextApiRequest, res: NextA
     const password = options?.password || process.env.BASIC_AUTH_PASSWORD
     const isAuthorized = user === username && pwd === password
 
-    if (!isAuthorized) {
-      res.setHeader('WWW-Authenticate', 'Basic realm="NodeJs"')
+    if (!isAuthorized)
       return res.status(401).send('Access denied')
-    }
 
     return callback(req, res)
   }
