89094a9d95e21932ce1372b5345dc75984a7be10
Bebe Jane <bjornthief@gmail.com>
2023-10-30 15:43:43 +0100

    updatez

--- src/hoc/withVercelCronAuthEdge.ts
+++ src/hoc/withVercelCronAuthEdge.ts
@@ -3,11 +3,10 @@ import { NextRequest, NextResponse } from 'next/server.js'
 export default function withVercelCronAuthEdge(callback: (req: NextRequest, res: NextResponse) => void): (req: NextRequest, res: NextResponse) => void {
 
   return async (req: NextRequest, res: NextResponse) => {
-    console.log(req.headers.get('authorization'), process.env.CRON_SECRET)
     if (!process.env.CRON_SECRET)
       throw new Error('CRON_SECRET not set in .env')
 
-    if (req.headers.get('authorization') === `Basic ${process.env.CRON_SECRET}`)
+    if (req.headers.get('authorization') === `Bearer ${process.env.CRON_SECRET}`)
       return callback(req, res)
     else
       return new Response('Access denied', { status: 401 })
