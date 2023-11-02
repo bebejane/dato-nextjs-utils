e8e9f2cac8645e45a95c09f946672de175ec86a8
Bebe Jane <bjornthief@gmail.com>
2023-11-02 10:53:56 +0100

    updatez

--- src/hoc/withRevalidate.ts
+++ src/hoc/withRevalidate.ts
@@ -1,28 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export const basicAuth = (req: NextApiRequest) => {
-
-  if (!process.env.BASIC_AUTH_USER || !process.env.BASIC_AUTH_PASSWORD)
-    throw new Error('BASIC_AUTH_USER or BASIC_AUTH_PASSWORD not set in .env')
-
-  const basicAuth = req.headers.authorization
-
-  if (!basicAuth)
-    return true;
-
-  const auth = basicAuth.split(' ')[1]
-  const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
-  return user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD
-}
-
 export default function withRevalidate(callback: (record: any, revalidate: (paths: string[]) => Promise<void>) => Promise<void>): (req: NextApiRequest, res: NextApiResponse) => void {
 
   return async (req: NextApiRequest, res: NextApiResponse) => {
 
-    if (req.method === 'GET' && req.query?.ping) {
-      console.log('ping')
+    if (req.method === 'GET' && req.query?.ping)
       return res.status(200).send('pong')
-    }
 
     if (!basicAuth(req))
       return res.status(401).send('Access denied')
@@ -63,4 +46,17 @@ export default function withRevalidate(callback: (record: any, revalidate: (path
   }
 }
 
+export const basicAuth = (req: NextApiRequest) => {
 
+  if (!process.env.BASIC_AUTH_USER || !process.env.BASIC_AUTH_PASSWORD)
+    throw new Error('BASIC_AUTH_USER or BASIC_AUTH_PASSWORD not set in .env')
+
+  const basicAuth = req.headers.authorization
+
+  if (!basicAuth)
+    return true;
+
+  const auth = basicAuth.split(' ')[1]
+  const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+  return user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD
+}
