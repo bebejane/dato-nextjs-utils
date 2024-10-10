b2102f06e442eab768efefbbea0eb430393ab554
Bebe Jane <bjornthief@gmail.com>
2024-10-10 17:10:56 +0200

    updatez

--- src/hoc/withVercelCronAuth.ts
+++ src/hoc/withVercelCronAuth.ts
@@ -7,8 +7,7 @@ export default function withVercelCronAuth(callback: (req: NextApiRequest, res: 
     if (!process.env.CRON_SECRET)
       throw new Error('CRON_SECRET not set in .env')
 
-    console.log(req.headers?.authorization)
-    if (req.headers?.authorization === `Basic ${process.env.CRON_SECRET}`)
+    if (req.headers?.authorization === `Bearer ${process.env.CRON_SECRET}`)
       return callback(req, res)
     else
       return res.status(401).send('Access denied')
